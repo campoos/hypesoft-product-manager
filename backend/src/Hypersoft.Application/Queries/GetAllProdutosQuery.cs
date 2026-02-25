7de1e23052509e22de341dddbb25f368444c065b
Campoos <joaosantos20071009@gmail.com>
2026-02-25 10:09:49 -0300

    feat(application): create GetAllProdutosQuery for listing products

---
+++ backend/src/Hypersoft.Application/Queries/GetAllProdutosQuery.cs
@@ -0,0 +1,11 @@
+using Hypersoft.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Hypesoft.Application.Queries.Produtos
+{
+    public class GetAllProdutosQuery : IResquest<List<Produtos>>
+    {
+
+    }
+}
