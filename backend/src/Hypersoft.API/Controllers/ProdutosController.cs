fa59f0a97ecf2010df52b84106d9ae5ca65d1fdc
Campoos <joaosantos20071009@gmail.com>
2026-02-25 13:39:46 -0300

    feat(products): add ProdutosController with Create and GetAll endpoints

---
+++ backend/src/Hypersoft.API/Controllers/ProdutosController.cs
@@ -0,0 +1,38 @@
+using Hypesoft.Application.Commands.Produtos;
+using Hypesoft.Application.DTOs.Produtos;
+using Hypesoft.Application.Queries.Produtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hypesoft.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProdutosController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoResponseDto>> Create([FromBody] ProdutoRequestDto produtoRequest)
+        {
+            var command = new CreateProdutoCommand(produtoRequest);
+            var resultado = await _mediator.Send(command);
+            return Ok(resultado);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProdutoResponseDto>>> GetAll()
+        {
+            var query = new GetAllProdutosQuery();
+            var resultado = await _mediator.Send(query);
+            return Ok(resultado);
+        }
+    }
+}
