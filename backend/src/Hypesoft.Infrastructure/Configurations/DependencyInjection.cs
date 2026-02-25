6a09bc389cfe8680f9a6c7430ed50d84db805ec4
Campoos <joaosantos20071009@gmail.com>
2026-02-25 20:03:41 -0300

    style(infrastructure): remove unnecessary comments in DependencyInjection

--- backend/src/Hypesoft.Infrastructure/Configurations/DependencyInjection.cs
+++ backend/src/Hypesoft.Infrastructure/Configurations/DependencyInjection.cs
@@ -9,16 +9,10 @@ namespace Hypesoft.Infrastructure.Configurations
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString, string databaseName)
         {
-            // MongoDB Context
             var mongoContext = new MongoContext(connectionString, databaseName);
             services.AddSingleton(mongoContext);
 
-            // Repositories
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
-            // services.AddScoped<ICategoriaRepository, CategoriaRepository>(); // se tiver categorias
-
-            // Serviços externos (ex: Keycloak, Email)
-            // services.AddHttpClient<KeycloakService>();
 
             return services;
         }
