23c79288aa39426e669f1b6e858fb601e4789b7d
Campoos <joaosantos20071009@gmail.com>
2026-02-25 01:00:59 -0300

    feat(domain): add IProdutoRepository interface

---
+++ backend/src/Hypersoft.Domain/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+using Hypesoft.Domain.Entities;
+
+namespace Hypesoft.Domain.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task<List<Produto>> GetAllAsync();
+        Task<Produto?> GetByIdAsync(string id);
+        Task<Produto> CreateAsync(Produto produto);
+        Task<Produto> UpdateAsync(Produto produto);
+        Task DeleteAsync(string id);
+    }
+}
