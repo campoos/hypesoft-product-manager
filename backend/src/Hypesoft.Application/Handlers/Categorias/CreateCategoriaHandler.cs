295526ab47e09cef32459d79bf99ad941fc97a66
Campoos <joaosantos20071009@gmail.com>
2026-02-26 18:41:53 -0300

    feat(handler): implement CreateCategoriaHandler

---
+++ backend/src/Hypesoft.Application/Handlers/Categorias/CreateCategoriaHandler.cs
@@ -0,0 +1,37 @@
+using Hypesoft.Domain.Entities;
+using Hypesoft.Domain.Repositories;
+using Hypesoft.Application.Commands.Categorias;
+using Hypesoft.Application.DTOs.Categorias;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Data.Common;
+using AutoMapper;
+
+namespace Hypesoft.Application.Handlers.Categorias
+{
+    public class CreateCategoriaHandler : IRequestHandler<CreateCategoriaCommand, CategoriaResponseDto>
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        private readonly IMapper _mapper;
+
+        public CreateCategoriaHandler(ICategoriaRepository categoriaRepository, IMapper mapper)
+        {
+            _categoriaRepository = categoriaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CategoriaResponseDto> Handle(CreateCategoriaCommand request, CancellationToken cancellationToken)
+        {
+            var categoria = new Categoria
+            {
+                Nome = request.Categoria.Nome
+            };
+
+            var criado = await _categoriaRepository.CreateAsync(categoria);
+
+            return _mapper.Map<CategoriaResponseDto>(criado);
+        }
+    }
+}
