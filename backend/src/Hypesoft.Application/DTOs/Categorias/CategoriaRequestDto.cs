1a77e200244fa5c366b7fde1ad8c0f656e42d068
Campoos <joaosantos20071009@gmail.com>
2026-02-26 17:26:32 -0300

    feat(dtos): request and response DTOs of Categoria added

---
+++ backend/src/Hypesoft.Application/DTOs/Categorias/CategoriaRequestDto.cs
@@ -0,0 +1,8 @@
+namespace Hypesoft.Application.DTOs.Produtos
+{
+    public class CategoriaRequestDto
+    {
+        public string Nome { get; set; } = null!;
+        
+    }
+}
---
+++ backend/src/Hypesoft.Application/DTOs/Categorias/CategoriaResponseDto.cs
@@ -0,0 +1,9 @@
+namespace Hypesoft.Application.DTOs.Produtos
+{
+    public class CategoriaResponseDto
+    {
+        public string Id { get; set; } = null!;
+        public string Nome { get; set; } = null!;
+
+    }
+}
