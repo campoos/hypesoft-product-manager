d2742d887f9f00004ad4f73b74fa90b3c54b363c
Campoos <joaosantos20071009@gmail.com>
2026-02-26 16:44:43 -0300

    feat(entities): create Categoria entity

---
+++ backend/src/Hypesoft.Domain/Entities/Categoria.cs
@@ -0,0 +1,31 @@
+using System;
+using Hypesoft.Domain.Exceptions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace Hypesoft.Domain.Entities
+{
+    public class Categoria
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; private set; }
+
+        public Categoria()
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+        }
+        
+        private string _nome = string.Empty;
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                    throw new DomainValidationException("Nome da categoria deve ter ao mínimo 3 caracteres.");
+                
+                _nome = value;
+            }
+        }
+    }
+}
