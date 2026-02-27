a1add04594a45e05b602be276b43c901793855fe
Campoos <joaosantos20071009@gmail.com>
2026-02-27 09:43:22 -0300

    feat(application): implement GetByIdHandler

---
+++ backend/src/Hypesoft.Application/Handlers/Categorias/GetByIdCategoriaHandler.cs
@@ -0,0 +1,35 @@
+
+using Hypesoft.Domain.Repositories;
+using Hypesoft.Application.Exceptions;
+using Hypesoft.Application.DTOs.Categorias;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Hypesoft.Application.Queries.Categorias;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace Hypesoft.Application.Handlers.Categorias
+{
+   public class GetByIdCategoriaHandler : IRequestHandler<GetByIdCategoriasQuery, CategoriaResponseDto>
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        private readonly IMapper _mapper;
+
+        public GetByIdCategoriaHandler(ICategoriaRepository categoriaRepository, IMapper mapper)
+        {
+            _categoriaRepository = categoriaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CategoriaResponseDto> Handle(GetByIdCategoriasQuery request, CancellationToken cancellationToken)
+        {
+            var categoria = await _categoriaRepository.GetByIdAsync(request.Id);
+
+            if (categoria == null)
+                throw new NotFoundException("Categoria não encontrada");
+
+            return _mapper.Map<CategoriaResponseDto>(categoria);
+        }
+    }
+}
