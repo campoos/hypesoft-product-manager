a9c776e7b1e885b74ccf7107483fb98cd9227303
Campoos <joaosantos20071009@gmail.com>
2026-02-27 13:42:27 -0300

    feat(application): add CategoriaResumoDto for nested category representation

---
+++ backend/src/Hypesoft.Application/DTOs/Categorias/CategoriaResumoDto.cs
@@ -0,0 +1,9 @@
+namespace Hypesoft.Application.DTOs.Categorias
+{
+    public class CategoriaResumoDto
+    {
+        public string Id { get; set; } = null!;
+        public string Nome { get; set; } = null!;
+
+    }
+}
