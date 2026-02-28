82ed952c706773b6375048393fcb7771b66b3e79
Campoos <joaosantos20071009@gmail.com>
2026-02-28 08:07:53 -0300

    feat(application): create GetDashboardDataQuery for dashboard summary

---
+++ backend/src/Hypesoft.Application/Queries/Dashboard/GetDashboardDataQuery.cs
@@ -0,0 +1,10 @@
+using MediatR;
+using Hypesoft.Application.DTOs.Dashboard;
+
+namespace Hypesoft.Application.Queries.Dashboard
+{
+    public class GetDashboardDataQuery : IRequest<DashboardDataDto>
+    {
+        
+    }
+}
