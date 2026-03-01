a8a623d12d2023e745a18f039bee7be342d8e500
Campoos <joaosantos20071009@gmail.com>
2026-03-01 18:09:09 -0300

    feat(frontend): modal component created

---
+++ frontend/src/components/forms/ProductModal.tsx
@@ -0,0 +1,130 @@
+import { useEffect, useState } from "react";
+import "./ProductModal.css";
+
+import { createProduct } from "../../../src/services/products.ts";
+import { fetchCategorias } from "../../../src/services/categories.ts";
+
+import type { ProductRequest } from "../../../src/services/products.tsx";
+import type { CategoriaResponse } from "../../../src/services/categories.ts";
+
+interface ProductModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSuccess?: () => void;
+}
+
+export default function ProductModal({ isOpen, onClose, onSuccess }: ProductModalProps) {
+
+    const [form, setForm] = useState<ProductRequest>({
+        nome: "",
+        descricao: "",
+        preco: 0,
+        quantidadeEmEstoque: 0,
+        categoriaId: "",
+    });
+
+    const [categorias, setCategorias] = useState<CategoriaResponse[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!isOpen) return;
+
+        fetchCategorias()
+        .then(data => setCategorias(data))
+        .catch(() => setError("Erro ao carregar categorias"));
+    }, [isOpen]);
+
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+
+    setForm(prev => ({
+      ...prev,
+        [name]: name === "preco" || name === "quantidadeEmEstoque"
+            ? Number(value)
+            : value
+        }));
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+        await createProduct(form);
+        onClose();
+        onSuccess?.();
+        } catch (err) {
+        setError("Erro ao criar produto");
+        }
+    };
+
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+        <div className="modal">
+
+            <header>
+            <h2>Criar Produto</h2>
+            <button onClick={onClose}>X</button>
+            </header>
+
+            <form onSubmit={handleSubmit}>
+
+            <input
+                name="nome"
+                placeholder="Nome"
+                value={form.nome}
+                onChange={handleChange}
+                required
+            />
+
+            <input
+                name="descricao"
+                placeholder="Descrição"
+                value={form.descricao}
+                onChange={handleChange}
+                required
+            />
+
+            <input
+                type="number"
+                name="preco"
+                placeholder="Preço"
+                value={form.preco}
+                onChange={handleChange}
+                required
+            />
+
+            <input
+                type="number"
+                name="quantidadeEmEstoque"
+                placeholder="Estoque"
+                value={form.quantidadeEmEstoque}
+                onChange={handleChange}
+                required
+            />
+
+            <select
+                name="categoriaId"
+                value={form.categoriaId}
+                onChange={handleChange}
+                required
+            >
+                <option value="">Selecione uma categoria</option>
+
+                {categorias.map(cat => (
+                <option key={cat.id} value={cat.id}>
+                    {cat.nome}
+                </option>
+                ))}
+            </select>
+
+            {error && <p className="error">{error}</p>}
+
+            <button type="submit">Criar</button>
+            </form>
+
+        </div>
+        </div>
+    );
+}
