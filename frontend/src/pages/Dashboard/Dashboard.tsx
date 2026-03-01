eaede36a6721e12933df9b892107454dcbb04f23
Campoos <joaosantos20071009@gmail.com>
2026-02-28 23:39:38 -0300

    feat(products): Add static cards to Dashboard with icons

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,18 @@
 import './Dashboard.css';
 import Header from '../../components/layout/header/Header.tsx'
 
+import iconStockValue from "../../assets/cards/coins.png"
+import iconTotalProducts from "../../assets/cards/boxes.png"
+import iconLowStock from "../../assets/cards/warning.png"
+
 export default function Dashboard() {
+
+  const cardsData = [
+    { icon: iconStockValue, title: "Valor Total do Estoque", value: "R$2.500,00" },
+    { icon: iconTotalProducts, title: "Total de Produtos", value: 350 },
+    { icon: iconLowStock, title: "Produtos com Estoque Baixo", value: 12 }
+  ];
+
   return (
     <div className="dashboard">
       <Header></Header>
@@ -18,8 +29,17 @@ export default function Dashboard() {
         <main className='main-content'>
           <h1>Dashboard</h1>
           <div className="cards-container">
-            <div className="card"></div>
-            <div className="card"></div>
+            {cardsData.map((card, index) => (
+              <div className="card" key={index}>
+                <div className="card-header">
+                  <div className="image-container">
+                    <img src={card.icon} alt={card.title} />
+                  </div>
+                  <h2>{card.title}</h2>
+                </div>
+                <p>{card.value}</p>
+              </div>
+            ))}
           </div>
         </main>
       </div>
