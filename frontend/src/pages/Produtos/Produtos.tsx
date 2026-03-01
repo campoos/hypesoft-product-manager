21ba819cc6daafe3f062ef7de198246dfc8f7fa0
Campoos <joaosantos20071009@gmail.com>
2026-03-01 15:45:48 -0300

    feat(frontend): add basic products page structure

--- frontend/src/pages/Produtos/Produtos.tsx
+++ frontend/src/pages/Produtos/Produtos.tsx
@@ -1,9 +1,92 @@
 import './Produtos.css';
+import { useEffect, useState } from 'react';
+
+import { fetchProductsFiltered } from '../../services/products.ts';
+import type { ProductResponse } from '../../services/products.ts';
+
+import Header from '../../components/layout/header/Header.tsx'
+import Sidebar from '../../components/layout/sidebar/Sidebar.tsx'
+
+import iconStockValue from "../../assets/cards/coins.png"
 
 export default function Produtos() {
+
+  const [dataLowStock, setDataLowStock] = useState<ProductResponse[] | null>(null);
+  const [errorLowStock, setErrorLowStock] = useState<string | null>(null);
+
+  useEffect(() => {
+
+    // Fetch dos produtos com estoque baixo
+    fetchProductsFiltered(undefined, undefined, undefined)
+      .then(data => {
+        if (data.length === 0) {
+          setDataLowStock([]);
+        } else {
+          setDataLowStock(data);
+        }
+      })
+      .catch(err => {
+        console.error("Erro ao buscar produtos com estoque baixo", err);
+        setErrorLowStock("Não foi possível carregar os produtos com estoque baixo");
+      });
+  }, []);
+
   return (
-    <>
-        <h1>Produtos</h1>
-    </>
+    <div className="dashboard">
+      <Header></Header>
+      <div className="body-container">
+
+        <Sidebar></Sidebar>
+
+        <main className='main-container'>
+          <div className="main-content">
+            <header>
+                <h1>Produtos</h1>
+                <button>
+                    <img src={iconStockValue} alt="" />
+                    <span>Criar</span>
+                </button>
+            </header>
+            
+            <div className="tabela-container">
+              <table className="tabela">
+                <thead className='header-container'>
+                  <tr className='header-line'>
+                    <th className="produto-id">Produto ID</th>
+                    <th className="produto-nome">Produto</th>
+                    <th className="produto-categoria">Categoria</th>
+                    <th className="produto-estoque">Estoque</th>
+                    <th className="produto-preco">Preço</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {errorLowStock && (
+                    <tr className='linha-mensagem'>
+                      <td colSpan={5} className="tabela-mensagem">{errorLowStock}</td>
+                    </tr>
+                  )}
+
+                  {dataLowStock && dataLowStock.length === 0 && !errorLowStock && (
+                    <tr className='linha-mensagem'>
+                      <td colSpan={5} className="tabela-mensagem">Não existem produtos em baixo estoque</td>
+                    </tr>
+                  )}
+
+                  {dataLowStock && dataLowStock.length > 0 && dataLowStock.map(produto => (
+                    <tr key={produto.id} className='linha-produto'>
+                      <td className='id-td'>{produto.id}</td>
+                      <td className='nome-td'>{produto.nome}</td>
+                      <td className='categoria-td'>{produto.categoria.nome}</td>
+                      <td className='quant-td'>{produto.quantidadeEmEstoque}</td>
+                      <td className='preco-td'>R${produto.preco.toLocaleString()}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
