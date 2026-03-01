415b50c06e9c6e4e310c528e3723704f4a8b80f4
Campoos <joaosantos20071009@gmail.com>
2026-02-28 22:33:00 -0300

    feat(frontend): add dashboard body structure

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -5,6 +5,20 @@ export default function Dashboard() {
   return (
     <div className="dashboard">
       <Header></Header>
+      <div className="body-container">
+        <aside className="sidebar">
+          <nav>
+            <ul>
+              <li><a href="/dashboard">Dashboard</a></li>
+              <li><a href="/products">Products</a></li>
+              <li><a href="/categories">Categories</a></li>
+            </ul>
+          </nav>
+        </aside>
+        <main className='main-content'>
+          <h1>main content aqui</h1>
+        </main>
+      </div>
     </div>
   );
 }
