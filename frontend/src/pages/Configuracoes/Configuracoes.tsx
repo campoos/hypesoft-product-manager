40297cc86d3ba3ebe756999378d78c312197917a
Campoos <joaosantos20071009@gmail.com>
2026-03-01 22:18:48 -0300

    feat(frontend): add settings page placeholder

--- frontend/src/pages/Configuracoes/Configuracoes.tsx
+++ frontend/src/pages/Configuracoes/Configuracoes.tsx
@@ -1,9 +1,23 @@
 import './Configuracoes.css';
 
+import Header from '../../components/layout/header/Header.tsx'
+import Sidebar from '../../components/layout/sidebar/Sidebar.tsx'
+
 export default function Configuracoes() {
   return (
-    <>
-        <h1>Configuracoes</h1>
-    </>
+    <div className="dashboard">
+      <Header></Header>
+      <div className="body-container">
+
+        <Sidebar></Sidebar>
+
+        <main className='main-container'>
+          <div className="main-content">
+            <h1>Configurações</h1>
+           
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
