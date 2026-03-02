5f947214ebc822ea1bf420cdc6d4268f2b03ec76
Campoos <joaosantos20071009@gmail.com>
2026-03-01 22:17:07 -0300

    feat(frontend): add categorias page placeholder

--- frontend/src/pages/Categorias/Categorias.tsx
+++ frontend/src/pages/Categorias/Categorias.tsx
@@ -1,9 +1,23 @@
 import './Categorias.css';
 
+import Header from '../../components/layout/header/Header.tsx'
+import Sidebar from '../../components/layout/sidebar/Sidebar.tsx'
+
 export default function Categorias() {
   return (
-    <>
-        <h1>Categorias</h1>
-    </>
+    <div className="dashboard">
+      <Header></Header>
+      <div className="body-container">
+
+        <Sidebar></Sidebar>
+
+        <main className='main-container'>
+          <div className="main-content">
+            <h1>Categorias</h1>
+           
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
