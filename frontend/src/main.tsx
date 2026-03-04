cc6b0de571f9d5d67615a8f5fe83d7561e97c91c
Campoos <joaosantos20071009@gmail.com>
2026-03-03 23:52:30 -0300

    feat(auth): wrap AppRouter with AuthWrapper in main.tsx to protect all routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './routes/AppRouter';
+import AuthWrapper from './components/auth/AuthWrapper.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
+    <AuthWrapper>
+      <AppRouter />
+    </AuthWrapper>
   </React.StrictMode>
 );
