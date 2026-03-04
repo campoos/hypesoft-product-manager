fc3a24efca55a4ee69b58acea4cad5881c9fa45b
Campoos <joaosantos20071009@gmail.com>
2026-03-04 13:03:59 -0300

    feat(frontend): implement list fetch in categoties page

--- frontend/src/pages/Categorias/Categorias.tsx
+++ frontend/src/pages/Categorias/Categorias.tsx
@@ -1,11 +1,85 @@
 import './Categorias.css';
+import { useEffect, useState } from 'react';
+
+import { fetchCategorias } from "../../services/categories.ts";
+import type { CategoriaResponse } from "../../services/categories";
 
 import Header from '../../components/layout/header/Header.tsx'
 import Sidebar from '../../components/layout/sidebar/Sidebar.tsx'
+import EditProductModal from '../../components/forms/editProduct/EditProductModal.tsx';
+import ProductModal from '../../components/forms/product/ProductModal.tsx';
+
+import { useLocation, useNavigate } from "react-router-dom";
+
+import createIcon from "../../assets/plus.png"
+
+import keycloak, { type KeycloakTokenParsed } from '../../auth/keycloak';
 
 export default function Categorias() {
+
+   const [role, setRole] = useState<string | null>(null);
+
+    
+    useEffect(() => {
+        if (keycloak.authenticated) {
+            const token: KeycloakTokenParsed | undefined = keycloak.tokenParsed;
+
+            const roles = token?.realm_access?.roles || [];
+
+            if (roles.includes("administrador")) {
+                setRole("administrador");
+            } else if (roles.includes("usuario")) {
+                setRole("usuario");
+            } else {
+                setRole("guest");
+            }
+        }
+    }, []);
+
+  const [categorias, setCategorias] = useState<CategoriaResponse[]>([]);
+
+  useEffect(() => {
+    fetchCategorias().then(setCategorias);
+  }, []);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.state?.openProductId) {
+      setSelectedProductId(location.state.openProductId);
+      setIsEditModalOpen(true);
+
+      navigate(location.pathname, { replace: true });
+    }
+  }, [location.state, navigate]);
+
+  const [selectedProductId, setSelectedProductId] = useState<string | null>(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);  
+
   return (
     <div className="dashboard">
+        <ProductModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            onSuccess={() => {
+                fetchCategorias().then(setCategorias);
+            }}
+        />
+        <EditProductModal
+          productId={selectedProductId}
+          isOpen={isEditModalOpen}
+          onClose={() => setIsEditModalOpen(false)}
+          onSuccess={() => {
+            fetchCategorias().then(setCategorias);
+          }}
+        />
+        {(isModalOpen || isEditModalOpen) && (
+          <div className='dark-background'></div>
+        )}
+
       <Header></Header>
       <div className="body-container">
 
@@ -13,8 +87,49 @@ export default function Categorias() {
 
         <main className='main-container'>
           <div className="main-content">
-            <h1>Categorias</h1>
-           
+            <header>
+                <h1>Categorias</h1>
+                <button onClick={() => setIsModalOpen(true)} disabled={role !== "administrador"}>
+                    <img src={createIcon} alt="" />
+                    <span>Criar</span>
+                </button>
+            </header>
+            
+            <div className="tabela-container">
+              <table className="tabela">
+                <thead className='header-container'>
+                  <tr className='header-line'>
+                    <th className="categoria-id">Categoria ID</th>
+                    <th className="categoria-nome">Categoria</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                  {categorias.length === 0 && (
+                    <tr className='linha-mensagem'>
+                      <td colSpan={5} className="tabela-mensagem">Não existem categorias a serem listados</td>
+                    </tr>
+                  )}
+
+                  {categorias.map(categoria => (
+                    <tr
+                      key={categoria.id}
+                      className={`linha-produto ${role !== "administrador" ? 'disabled' : ''}`} 
+                      onClick={() => {
+                        if(role !== "administrador"){
+                          return
+                        }
+                        setSelectedProductId(categoria.id);
+                        setIsEditModalOpen(true);
+                      }}
+                    >
+                      <td className='id'>{categoria.id}</td>
+                      <td className='nome'>{categoria.nome}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </main>
       </div>
